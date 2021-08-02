8beddf4034fb8254bade18f2b00d8bc741c96fdc
Oleh-N-Lunin <67375261+Oleh-N-Lunin@users.noreply.github.com>
2021-08-02 16:47:42 +0300

    Finished hw3

--- src/App.js
+++ src/App.js
@@ -8,14 +8,23 @@ import styles from "./components/FormStyles/FormStyles.module.css";
 
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const getStorageContact = localStorage.getItem('contact');
+    const parseStorageContact = JSON.parse(getStorageContact);
+    if (getStorageContact) {
+      this.setState({ contacts: parseStorageContact });
+    }
+  }
 
   addContact = contact => {
     this.setState({
